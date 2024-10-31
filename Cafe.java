82fe696cff201c0fe097f44e88621b39cdcd35c0
govenERR <lfairman@smith.edu>
2024-10-30 22:24:27 -0400

    Update Cafe.java
    SHE IS WRITTEN

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,54 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
-
-    public Cafe() {
+public class Cafe extends Building {
+    private int nCoffeeOunces;
+    private int nSugarPackets;
+    private int nCreams;
+    private int nCups;
+/*
+ * Creates a new Cafe
+ */
+    public Cafe(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups, String name, String address, int nFloors) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
         System.out.println("You have built a cafe: ☕");
+    }
+/*
+ * Lowers the inventory reflecting the size and components of the coffee
+ */
+    public void sellCoffee(int size, int sugar, int creams) {
+        if (this.nCoffeeOunces < size) {
+            restock(size + 8, 0, 0, 0);
+        }
+        this.nCoffeeOunces += -size;
+        if (this.nSugarPackets < sugar) {
+            restock(0, sugar + 5, 0, 0);
+        }
+        this.nSugarPackets += -sugar;
+        if (this.nCreams < creams) {
+            restock(0, 0, creams + 3, 0);
+        }
+        this.nCreams += -creams;
+        if (this.nCups == 0) {
+            restock(0,0,0,5);
+        }
+        this.nCups += -1;
+    }
+/*
+ * Refills the inventory of the coffee shop
+ */
+    private void restock(int ncoffee, int nsugar, int ncreams, int ncups) {
+        this.nCoffeeOunces += ncoffee;
+        this.nSugarPackets += nsugar;
+        this.nCreams += ncreams;
+        this.nCups += ncups;
     }
     
     public static void main(String[] args) {
-        new Cafe();
+        Cafe compass = new Cafe(12,6,6, 4, "Compass Cafe", "7 Neilson Street", 1);
+        System.out.println(compass.getAddress());
     }
     
 }
